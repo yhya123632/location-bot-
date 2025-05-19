a02f3749c616ed453a0d08e71668a84839e20853
yhya123632 <qnaahd426@gmail.com>
2025-05-19 17:08:28 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.webviewapp;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://yhya123632.github.io/one-phone-landing/");
+    }
+}
